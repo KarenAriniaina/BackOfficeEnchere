a0220a430cc6c3d9c1c4a723dde7b3c400e19c6f
KarenAriniaina <99282731+KarenAriniaina@users.noreply.github.com>
2023-01-20 08:37:19 +0300

    izy rehetra mitambatra

---
+++ jsDev/updatecategorie.js
@@ -0,0 +1,54 @@
+window.addEventListener("load", function () {
+    if (this.localStorage.getItem("token") == null && this.localStorage.getItem("idAdmin") == null) {
+        window.location.assign("index.html");
+    } else {
+        let parame = new URLSearchParams(window.location.search);
+        var id;
+        for (const param of parame) {
+            id = param[1];
+            //console.log(id);
+        }
+        function sendData() {
+            var xhr;
+            try { xhr = new ActiveXObject('Msxml2.XMLHTTP'); }
+            catch (e) {
+                try { xhr = new ActiveXObject('Microsoft.XMLHTTP'); }
+                catch (e2) {
+                    try { xhr = new XMLHttpRequest(); }
+                    catch (e3) { xhr = false; }
+                }
+            }
+            var formData = new FormData(form);
+            xhr.addEventListener("load", function (event) {
+                var retour = JSON.parse(xhr.responseText);
+                console.log(retour);
+                if (retour.status == true) {
+                    alert(retour.message);
+                    window.location.assign("GestionCategorie.html");
+                }
+                else {
+                    if (retour.message == "Vous n'etes pas connecté") {
+                        localStorage.removeItem("idAdmin");
+                        localStorage.removeItem("token");
+                        window.location.assign("index.html");
+                    }
+                    else {
+                        alert(retour.message);
+                    }
+                }
+            });
+            xhr.addEventListener("error", function (event) {
+                alert('Oups! Quelque chose s\'est mal passé.');
+            });
+            xhr.open("POST", "http://localhost:8080/Categorie/"+id,true);
+            xhr.setRequestHeader("idAdmin", localStorage.getItem('idAdmin'));
+            xhr.setRequestHeader("token", localStorage.getItem('token'));
+            xhr.send(formData);
+        }
+        var form = document.getElementById("myForm");
+        form.addEventListener("submit", function (event) {
+            event.preventDefault();
+            sendData();
+        });
+    }
+});
